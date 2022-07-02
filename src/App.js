2a21ceaf0dac54b5c9c5ff03134e131311a8c624
ErickCosta98 <74620461+ErickCosta98@users.noreply.github.com>
2022-07-01 23:52:53 -0500

    las update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 
-// Funcion para agregar una tarea
-// function addTask(e) {
-//   e.preventDefault();
-//   const task = document.getElementById('task').value;
-//   const list = document.getElementById('list');
-//   const li = document.createElement('li');
-//   // Agregar evento para borrar la tarea
-//   li.addEventListener('click', deleteTask);
-//   li.innerHTML = task;
-//   list.appendChild(li);
-// }
-
-// Funcion para borrar una tarea seleccionada
-// function deleteTask(e) {
-//   e.target.remove();
-// }
 
 //funcion para mostrar los pokemon en las tarjetas de la pagina principal desde la pokeapi
 function getPokemon() {
-  const url = 'https://pokeapi.co/api/v2/pokemon?offset=20&limit=20';
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'; //offset=20&limit=20
   fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -28,30 +12,74 @@ function getPokemon() {
       pokemons.forEach(pokemon => {
         const card = document.createElement('div');
         card.classList.add('card');
+        const cardDetails = document.createElement('div');
+        cardDetails.classList.add('card-details');
         const img = document.createElement('img');
         img.className = 'card-img-top';
         img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`;
         const name = document.createElement('h2');
         name.innerHTML = pokemon.name;
-        card.appendChild(img);
-        card.appendChild(name);
+        cardDetails.appendChild(img);
+        cardDetails.appendChild(name);
+        card.addEventListener('click', (e) => {
+          getPokemonDetail(e,pokemon.url);
+        });
+        card.appendChild(cardDetails);
         document.getElementById('pokemon').appendChild(card);
       });
     }).catch(error => console.log(error));
 }
+// funcion para mostrar los detalles de los pokemon extendiendo el card de la pagina principal
+function getPokemonDetail(e,url) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const pokemon = data;
+      const card = document.createElement('div');
+      card.classList.add('card-more-details');
+      const type = document.createElement('p');
+      type.innerHTML = pokemon.types.map(type => type.type.name).join(', ');
+      const height = document.createElement('p');
+      height.innerHTML = `Height: ${pokemon.height}`;
+      const weight = document.createElement('p');
+      weight.innerHTML = `Weight: ${pokemon.weight}`;
+      card.appendChild(type);
+      card.appendChild(height);
+      card.appendChild(weight);
+      e.target.appendChild(card);
+    }).catch(error => console.log(error));
+}
 
-getPokemon()
+//funcion mostrar lista de generaciones de pokemon
+function getGenerations() {
+  const url = 'https://pokeapi.co/api/v2/generation/';
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const generations = data.results;
+      generations.forEach(generation => {
+        const option = document.createElement('option');
+        option.innerHTML = generation.name;
+        document.getElementById('generations').appendChild(option);
+      });
+    }).catch(error => console.log(error));
+}
+//funcion mostrar lista de tipos de pokemon
 
+getPokemon();
+getGenerations();
 function App() {
   return (
-    <div className="App" >
-        {/* <h2>Pokemons</h2> */}
-      <div className="App-header" id='pokemon'>
+      <div className="App-header" >
+        <span>Pokemons</span>
+        <select id="generations">
+         </select>
+        <div className="cards" id='pokemon'>
+        </div>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
         
         </div>
-    </div>
   );
 }
 
